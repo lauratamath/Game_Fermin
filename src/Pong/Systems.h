948850ea6c36c426cd259e7e19d3baad3c0e7f7c
Levi Tamath <tam19365@uvg.edu.gt>
2023-11-12 22:32:49 -0600

    spriteSheet

--- src/Pong/Systems.h
+++ src/Pong/Systems.h
@@ -490,7 +490,7 @@ class PlayerSetupSystem : public SetupSystem {
       scene->player = new Entity(scene->r.create(), scene);
       scene->player->addComponent<TransformComponent>(x, y);
       auto& s = scene->player->addComponent<SpriteComponent>(
-        "Sprites/Cat/SpriteSheet.png",
+        "Sprites/Cat/FerminSpriteSheet.png",
         0, 0,
         spriteSize,
         8,
